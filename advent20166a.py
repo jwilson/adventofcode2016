7c3cc823aee4b40b0f99cf3425a6dc5d9bd038b3
Josh <josh@invaware.com>
2016-12-06 08:42:03 -0500

    day 6

---
+++ advent20166a.py
@@ -0,0 +1,15 @@
+input = """""".splitlines()
+import string
+columns = []
+for col in range(0,8):
+	column = {}
+	for letter in string.ascii_lowercase:
+		column[letter] = 0
+	columns.append(column)
+for line in input:
+	col = 0
+	for c in line:
+		columns[col][c] += 1
+		col += 1
+for column in columns:
+	print sorted(column.items(), key=lambda (k, v): (-v, k))[0]
